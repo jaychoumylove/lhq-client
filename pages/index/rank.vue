90946472a048a64a463b45e3a507d4d27f4a3205
zlf <2878253979@qq.com>
2020-09-15 11:35:32 +0800

    用户页面及排行页面初始

--- pages/index/rank.vue
+++ pages/index/rank.vue
@@ -1,8 +1,288 @@
 <template>
+	<view class="container">
+		<view class="top-info">
+			<view class="rewards space-between">
+				<view class="money">2000元</view>
+				<view class="rule">分红规则</view>
+			</view>
+			<view class="title">今日分红池</view>
+			<view class="top-title space-between">
+				<view class="">榜单</view>
+				<view class="">积分（个）+前3分红（元）</view>
+			</view>
+		</view>
+
+
+		<!-- 列表 -->
+		<view class="list-container">
+			<view class="item">
+				<view class="rank-left">
+					<view class="rank-num flex-set">
+						<image class="icon" src="https://mmbiz.qpic.cn/mmbiz_png/w5pLFvdua9HhvlXURtbJbFvRVwdINYhHcvbxyK2p9RfyBWvElE6xwtFt3PoIwISs0vLHUhmiaKvhnIL6kzh38uA/0"
+						 mode=""></image>
+					</view>
+					<view class="avatar-warp">
+						<image class="avatar" :src="item.user.avatarurl || AVATAR" mode="aspectFill"></image>
+					</view>
+					<view class="user-name">
+						{{item.user.nickname || NICKNAME}}
+					</view>
+				</view>
+				<view class="rank-right">
+					<view>{{item.hot || '1111'}}</view>
+				</view>
+			</view>
+			<view class="item">
+				<view class="rank-left">
+					<view class="rank-num flex-set">
+						<image class="icon" src="https://mmbiz.qpic.cn/mmbiz_png/w5pLFvdua9HhvlXURtbJbFvRVwdINYhHcvbxyK2p9RfyBWvElE6xwtFt3PoIwISs0vLHUhmiaKvhnIL6kzh38uA/0"
+						 mode=""></image>
+					</view>
+					<view class="avatar-warp">
+						<image class="avatar" :src="item.user.avatarurl || AVATAR" mode="aspectFill"></image>
+					</view>
+					<view class="user-name">
+						{{item.user.nickname || NICKNAME}}
+					</view>
+				</view>
+				<view class="rank-right">
+					<view>{{item.hot || '1111'}}</view>
+				</view>
+			</view>
+			<view class="item">
+				<view class="rank-left">
+					<view class="rank-num flex-set">
+						<image class="icon" src="https://mmbiz.qpic.cn/mmbiz_png/w5pLFvdua9HhvlXURtbJbFvRVwdINYhHcvbxyK2p9RfyBWvElE6xwtFt3PoIwISs0vLHUhmiaKvhnIL6kzh38uA/0"
+						 mode=""></image>
+					</view>
+					<view class="avatar-warp">
+						<image class="avatar" :src="item.user.avatarurl || AVATAR" mode="aspectFill"></image>
+					</view>
+					<view class="user-name">
+						{{item.user.nickname || NICKNAME}}
+					</view>
+				</view>
+				<view class="rank-right">
+					<view>{{item.hot || '1111'}}</view>
+				</view>
+			</view>
+			<view class="item" v-for="(item,index) in userRank" :key="index">
+				<view class="rank-left">
+					<view class="rank-num flex-set">
+						<image class="icon" v-if="index==0" src="https://mmbiz.qpic.cn/mmbiz_png/w5pLFvdua9HhvlXURtbJbFvRVwdINYhHcvbxyK2p9RfyBWvElE6xwtFt3PoIwISs0vLHUhmiaKvhnIL6kzh38uA/0"
+						 mode=""></image>
+						<image class="icon" v-else-if="index==1" src="https://mmbiz.qpic.cn/mmbiz_png/w5pLFvdua9HhvlXURtbJbFvRVwdINYhHREheWFHXGbHGf3unhB8zN0siaVwoZzictZOjBZzhyR5ibvLFMIPwibSWDQ/0"
+						 mode=""></image>
+						<image class="icon" v-else-if="index==2" src="https://mmbiz.qpic.cn/mmbiz_png/w5pLFvdua9HhvlXURtbJbFvRVwdINYhHibnaZud2z8MHeCCXWDicbgiajDCV5ke7vaCkk0040I7m4OVeGPIAibnpbA/0"
+						 mode=""></image>
+						<view v-else>{{index+1}}</view>
+					</view>
+					<view class="avatar-warp">
+						<image class="avatar" :src="item.user.avatarurl || AVATAR" mode="aspectFill"></image>
+					</view>
+					<view class="user-name">
+						{{item.user.nickname || NICKNAME}}
+					</view>
+				</view>
+				<view class="rank-right">
+					<view>{{item.hot || '1111'}}</view>
+				</view>
+			</view>
+		</view>
+
+		<!-- 我的 -->
+		<view class="my-container">
+			<view class="rank-num">
+				<view>{{myInfo.rank || 'no'}}</view>
+			</view>
+			<view class='avatar-wrap'>
+				<image class="avatar" :src="$app.getData('userInfo').avatarurl || AVATAR" mode="aspectFill"></image>
+			</view>
+			<view class="text-container">
+				<view>
+					{{$app.getData('userInfo').nickname || NICKNAME}}
+				</view>
+
+			</view>
+			<view class="count">{{myInfo.score||'1111'}}</view>
+		</view>
+	</view>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				userRank: [],
+				page: 1,
+				myInfo: '',
+				rankInfo: '',
+				AVATAR: this.$app.getData('AVATAR'),
+				NICKNAME: this.$app.getData('NICKNAME'),
+			};
+		},
+		onLoad(option) {
+			this.loadData()
+		},
+		onReachBottom() {
+			this.page++
+			this.loadData()
+		},
+		methods: {
+
+			loadData() {
+				if (this.page > 10) return
+
+				this.$app.request(this.$app.API.USER_RANK, {
+					starid: this.starid,
+					page: this.page,
+				}, res => {
+					this.rankInfo = res.data.rankInfo
+					this.myInfo = res.data.mymyInfo
+					if (this.page == 1) {
+						this.userRank = res.data.list
+					} else {
+						this.userRank = this.userRank.concat(res.data.list)
+					}
+				})
+			},
+		}
+	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.container {
+		.top-info{
+			padding: 40rpx 40rpx 0rpx 40rpx;
+			.rewards{
+				.money{
+					font-size: 40rpx;
+					font-weight: bold;
+					color: #000000;
+				}
+				.rule{
+					font-size: 28rpx;
+					color: $text-color-2;
+				}
+				.rule::after {
+					content: "\e6e5";
+					font-family: "iconfont" !important;
+					padding-left: 10upx;
+					color: #f00;
+				}
+			}
+			.title{
+				padding: 20rpx 0;
+				font-size: 28rpx;
+				color: $text-color-2;
+			}
+			.top-title{
+				padding: 20rpx 0;
+				font-size: 22rpx;
+				color: $text-color-2;
+			}
+		}
+
+		.list-container {
+			padding: 0 20rpx;
+
+			.item {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				margin: 40rpx 0;
+				flex-direction: row;
+
+				.rank-left {
+					display: flex;
+					align-items: center;
+					flex: 1 1 0%;
+
+					.rank-num {
+						width: 30rpx;
+						height: 30rpx;
+						margin: 0 10rpx;
+					}
+
+					.avatar-warp {
+						image {
+							border-radius: 50%;
+							width: 60rpx;
+							height: 60rpx;
+							margin: 0 10rpx;
+						}
+
+					}
+
+					.user-name {
+						margin: 0 10rpx;
+					}
+
+				}
+
+				.rank-right {
+					display: flex;
+					align-items: cnter;
+					margin-right: 20rpx;
+				}
+			}
+		}
+
+		.my-container {
+			position: fixed;
+			bottom: 0;
+			width: 100%;
+			height: 100upx;
+			display: flex;
+			align-items: center;
+			background-color: #FFF;
+			border-top: 2rpx solid #f2f3f4;
+
+			.rank-num {
+				text-align: center;
+				width: 100upx;
+
+				.icon {
+					width: 50upx;
+					height: 50upx;
+				}
+			}
+
+			.avatar-wrap {
+				position: relative;
+
+				.avatar {
+					width: 80upx;
+					height: 80upx;
+					border-radius: 50%;
+				}
+
+				.headwear {
+					width: 150%;
+					height: 150%;
+				}
+			}
+
+			.text-container {
+				margin: 0 20upx;
+				width: 350upx;
+
+				image {
+					width: 72rpx;
+					height: 30rpx;
+				}
+
+				.bottom-text {
+					display: flex;
+					align-items: center;
+					color: #FCF4F5;
+				}
+			}
+
+			.count {
+				margin-left: 30upx;
+				color: #ff8421;
+			}
+		}
+	}
 </style>
--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,22 +1,398 @@
 <template>
-	<view>
+	<view class="container">
+		<view class="top-content-container">
+			<view class="row userinfo">
+				<view class="get_user_info">
+					<button style="overflow: visible;" open-type="getUserInfo" @getuserinfo="getUserInfo">
+						<view class="avatar">
+							<view class="avatarurl">
+								<image :src="userInfo.avatarurl" mode="aspectFill"></image>
+								<view class="tips">点击获取</view>
+							</view>
+							<image class="headwear position-set" v-if="userInfo.curHeadwear&&userInfo.curHeadwear.img" :src="userInfo.curHeadwear.img"></image>
+						</view>
+					</button>
+				</view>
+
+				<view class="info-content">
+					<view class="item-line top">
+						<view class="username">{{userInfo.nickname}}</view>
+					</view>
+					<view class="item-line">
+						<view class="flex-set" v-if="userInfo.id" @tap="$app.copy(userInfo.id*1234)">
+							<text style="font-weight: bold; padding-right: 10rpx;">ID:{{userInfo.id*1234}}</text> 复制
+						</view>
+
+					</view>
+					
+				</view>
+			</view>
+		</view>
+		
+		<view class="function-container-list">
+			<view class="list-item" v-if="$app.getData('config').version != $app.getData('VERSION')" @tap="copy()">
+				<view class="left-wrap">
+					<view class="text">我的钱包</view>
+				</view>
+				<view class="right-wrap iconfont iconjiantou"></view>
+			</view>
+			<view class="list-item" @tap="$app.goPage('/pages/game/game?type=1')" v-if="$app.getData('config').version != $app.getData('VERSION')">
+				<view class="left-wrap">
+					<view class="text">我的积分</view>
+				</view>
+				<view class="right-wrap iconfont iconjiantou"></view>
+			</view>
+			<view class="list-item" @tap="$app.goPage('/pages/game/game?type=1')" v-if="$app.getData('config').version != $app.getData('VERSION')">
+				<view class="left-wrap">
+					<view class="text">我的专属二维码</view>
+				</view>
+				<view class="right-wrap iconfont iconjiantou"></view>
+			</view>
+			<!-- <button open-type="contact" :session-from="$app.getData('userInfo').id">
+				<view class="list-item">
+					<view class="left-wrap">
+						<view class="text">联系客服</view>
+					</view>
+					<view class="right-wrap iconfont iconjiantou"></view>
+				</view>
+			</button> -->
+		</view>
+		
+		<view class="task-container">
+			<view class="task-title">常规任务</view>
+		</view>
 		
 	</view>
 </template>
 
 <script>
+	import badgeComponent from "@/components/badgeComponent.vue"
+	import btnComponent from "@/components/btnComponent.vue"
+	import modalComponent from "@/components/modalComponent.vue"
 	export default {
+		components: {
+			badgeComponent,
+			modalComponent,
+			btnComponent
+		},
 		data() {
 			return {
-				
+				userInfo: {},
+				userCurrency: {},
+				userID: '',
+				modal: '',
+			};
+		},
+		onLoad() {},
+		onShow() {
+
+			this.userInfo = {
+				avatarurl: this.$app.getData('userInfo')['avatarurl'] || this.$app.getData('AVATAR'),
+				nickname: this.$app.getData('userInfo')['nickname'] || this.$app.getData('NICKNAME'),
+				id: this.$app.getData('userInfo')['id'] || '123456',
 			}
+			
+			this.userStar = this.$app.getData('userStar') || {}
+
+			this.$app.request(this.$app.API.USER_CURRENCY, {}, res => {
+				this.$app.setData('userCurrency', res.data)
+				this.userCurrency = this.$app.getData('userCurrency')
+			})
+
+			this.getInfo();
+		
+		},
+		onShareAppMessage(e) {
+			const shareType = e.target && e.target.dataset.share
+			return this.$app.commonShareAppMessage(shareType)
 		},
 		methods: {
-			
+			getInfo(){
+				this.$app.request(this.$app.API.USER_INFO, {}, res => {
+					this.userInfo = res.data;
+					this.$app.setData('userInfo', res.data, true)
+				})
+			},
+			copy() {
+				uni.setClipboardData({
+					data: this.$app.getData('config').kefu,
+					success: () => {
+						this.$app.toast("微信号已复制\n请到微信中添加客服为好友")
+					}
+				})
+			},
+			/**
+			 * 保存用户详细信息
+			 */
+			getUserInfo(e) {
+				const userInfo = e.detail.userInfo
+				if (userInfo) {
+					this.$app.request(this.$app.API.USER_SAVEINFO, {
+						iv: e.detail.iv,
+						encryptedData: e.detail.encryptedData,
+					}, res => {
+						if (res.data.token) this.$app.token = res.data.token
+						this.$app.request('page/app', {}, res => {
+							this.$app.setData('userCurrency', res.data.userCurrency)
+							this.$app.setData('userInfo', res.data.userInfo)
+							this.$app.setData('config', res.data.config)
+
+							this.userInfo = res.data.userInfo
+							this.$app.toast('更新成功')
+						})
+
+					}, 'POST', true)
+				}
+			},
 		}
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.container {
+		// padding: 20upx;
+		background-color: #f5f5f5;
 
+		.top-content-container {
+
+			padding: 20upx 30upx;
+			position: relative;
+			z-index: 2;
+
+			.row.userinfo {
+				display: flex;
+				border-radius: 30upx;
+				background-image: url(https://mmbiz.qpic.cn/mmbiz_png/w5pLFvdua9FmibDk8LrMNNib025upafEqqaB6iazPjm7Yajdx5R3LVxpBdicE4pfiarQYCicw5qHVW5kB0RXV0Su0Ukg/0);
+				background-size: 100% 100%;
+				padding: 30rpx 20rpx 40rpx 20rpx;
+
+				.get_user_info {
+					display: flex;
+					align-items: center;
+					padding-left: 20rpx;
+				}
+
+				.avatar {
+
+					.avatarurl {
+						position: relative;
+						overflow: hidden;
+						border-radius: 50%;
+						width: 140upx;
+						height: 140upx;
+
+						z-index: 1;
+
+					}
+
+
+				}
+
+				.info-content {
+					padding-left: 30rpx;
+					display: flex;
+					flex: 1 0%;
+					justify-content: space-around;
+					flex-direction: column;
+					font-size: 26upx;
+
+					.tips {
+						justify-content: space-between;
+						font-size: 22rpx;
+					}
+
+					.item-line {
+						width: 90%;
+						display: flex;
+						align-items: center;
+						padding: 10rpx 0;
+
+						.username {
+							font-weight: 700;
+							font-size: 30upx;
+							margin-right: 8upx;
+							color: #5F6176;
+							max-width: 300rpx;
+						}
+
+						image.vip {
+							width: 38upx;
+							height: 38upx;
+							margin-top: 4upx;
+							margin-right: 6upx;
+						}
+
+						.vip-expire {
+							font-size: 18upx;
+							color: #999;
+							margin-top: 12upx;
+						}
+
+						.id-content {
+							border-radius: 20upx;
+							font-size: 22upx;
+							background-color: #FDDE2F;
+							padding: 0 10upx;
+							color: #5F6176;
+							margin: 0 10upx;
+						}
+
+						
+
+						
+					}
+
+					.item-line.bottom {
+						font-size: 20upx;
+					}
+				}
+			}
+
+		}
+
+		.item-wrap {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			margin: 0 20upx;
+			padding-top: 20upx;
+
+			.left-wrap {
+				font-size: 30upx;
+				display: flex;
+				align-items: center;
+
+				.icon {
+					width: 63upx;
+					height: 24upx;
+					margin-right: 10upx;
+				}
+
+				.tips {
+					color: #999;
+					margin: 0 20upx;
+					font-size: 26upx;
+
+					.highlight {
+						color: #345;
+						font-weight: 700;
+					}
+
+				}
+			}
+
+			.right-wrap {
+				font-size: 22upx;
+				color: #999;
+			}
+		}
+
+		
+
+		.function-container-list {
+			padding: 0 20rpx;
+			background-color: #FFFFFF;
+
+			.red {
+				color: red;
+			}
+
+			.list-item {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				margin: 0 20upx;
+				padding: 20upx 0;
+				border-bottom: 1rpx solid #f5f5f5;
+				color: #5F6176;
+
+				.left-wrap {
+					display: flex;
+					align-items: center;
+
+					image {
+						width: 40upx;
+						height: 40upx;
+						margin-right: 30upx;
+					}
+				}
+
+				.right-wrap {
+					font-size: 22upx;
+				}
+			}
+		}
+
+		.task-container {
+			height: 100%;
+			padding: 20upx 40upx;
+			background-color: #FFFFFF;
+
+			.task-title {
+				font-size: 28upx;
+				font-weight: bold;
+				color: #5F6176;
+			}
+		}
+
+		.modal-container {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			padding: 40upx;
+			height: 100%;
+			justify-content: center;
+
+			.title {
+				font-size: 36upx;
+				font-weight: 700;
+			}
+
+			.desc {
+				display: flex;
+				align-items: center;
+				padding-top: 10upx;
+
+				.modal-color {
+					font-weight: 550;
+				}
+
+				image {
+					width: 30rpx;
+					height: 30rpx;
+					margin: 0 5rpx;
+				}
+			}
+
+			input {
+				color: #5F6176;
+				border-radius: 30upx;
+				height: 90rpx;
+				background: #ffd3dc;
+				width: 88%;
+				padding: 23rpx 40rpx;
+				margin: 40rpx 0 0 0;
+
+			}
+
+			.btn-wrap {
+				margin: 40upx 0;
+				text-align: center;
+				display: flex;
+				width: 100%;
+				justify-content: space-around;
+				padding: 0 20upx;
+			}
+
+			.btn {
+				font-size: 30upx;
+				font-weight: 600;
+				height: 80rpx;
+				width: 300rpx;
+
+			}
+
+		}
+
+	}
 </style>
