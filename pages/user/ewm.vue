a655eabf0ce0c30b2255d32c351c2f88e46b15ff
zlf <2878253979@qq.com>
2020-09-16 16:32:56 +0800

    二维码页面修改

--- pages/user/ewm.vue
+++ pages/user/ewm.vue
@@ -18,6 +18,9 @@
 				url: 'https://mmbiz.qpic.cn/mmbiz_png/w5pLFvdua9E6PCGc2OHpnROtW32Yh9zQZz84fibamQTPtFmJXMWLt1XUVM1oWvcQL9O8Y8NFWVricajJomgEpIWw/0',
 			};
 		},
+		onShow() {
+			this.loadData()
+		},
 		methods: {
 			save() {
 				console.log('url:', this.url);
@@ -50,9 +53,11 @@
 					}
 				});
 			},
-			created() {
-
-			}
+			loadData() {
+				this.$app.request('user/ewm', {}, res => {
+					this.url = res.data;
+				})
+			},
 		}
 	}
 </script>
