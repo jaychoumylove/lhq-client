cac5db9e874a7903ae6af1a5e86fa87ddae85dcc
caixian <qq52577517@163.com>
2020-09-17 14:57:39 +0800

    绘图完成

--- pages/user/ewm.vue
+++ pages/user/ewm.vue
@@ -5,6 +5,10 @@
 				<image class="receive-qrcode-img" :src="url" mode="widthFix" @longtap="toSave"></image>
 			</view>
 		</view>
+		
+		<view class="canvas-container flex-set">
+			<canvas canvas-id='mycanvas' class="canvas"></canvas>
+		</view>
 	</view>
 </template>
 
@@ -13,6 +17,8 @@
 		data() {
 			return {
 				url: '',
+				modal: "",
+				cover: 'https://mmbiz.qpic.cn/mmbiz_png/w5pLFvdua9E6PCGc2OHpnROtW32Yh9zQZz84fibamQTPtFmJXMWLt1XUVM1oWvcQL9O8Y8NFWVricajJomgEpIWw/0',
 			};
 		},
 		onShow() {
@@ -45,7 +51,8 @@
 					content: '确定要保存图片吗',
 					success: e => {
 						if (e['confirm']) {
-							this.save();
+							// this.save();
+							this.qrR();
 						}
 					}
 				});
@@ -55,11 +62,69 @@
 					this.url = res.data;
 				})
 			},
+			getLocalImg(src, callback) {
+				uni.getImageInfo({
+					src,
+					success: function(res) {
+						callback && callback(res.path)
+					}
+				})
+			},
+			qrR() {
+				var rate = this.$app.getData('sysInfo').windowWidth / 375 / 2.0
+				let system_info = uni.getSystemInfoSync();
+				let ctx = uni.createCanvasContext('mycanvas');
+				
+				this.getLocalImg(this.cover, src => {
+					ctx.drawImage(src, 0, 0, 480*rate, 854*rate);
+					// 二维码
+					this.getLocalImg(this.url, src => {
+						ctx.save() //保存当前的绘图上下文。
+						ctx.beginPath() //开始创建一个路径
+						ctx.arc(240 * rate, 520 * rate, 50 * rate, 0, 2 * Math.PI, false) //画一个圆形裁剪区域
+						ctx.clip() //裁剪
+						ctx.drawImage(src, 190 * rate, 470 * rate, 100 * rate, 100 * rate);
+						ctx.restore() //恢复之前保存的绘图上下文
+						// 绘制
+						ctx.draw(false, () => {
+							// 保存到临时图片
+							uni.canvasToTempFilePath({
+								canvasId: 'mycanvas',
+								success: res => {
+									uni.saveImageToPhotosAlbum({
+										filePath: res.tempFilePath,
+										success: () => {
+											this.$app.toast('保存成功', 'success')
+										}
+									});
+								}
+							}, this);
+						})
+					})
+				})
+			},
 		}
 	}
 </script>
 
 <style lang="scss">
+	.canvas-container {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		z-index: 9;
+		background-color: rgba(0, 0, 0, .9);
+		flex-direction: column;
+	
+		.canvas {
+			width: 480upx;
+			height: 854upx;
+			margin-top: 40rpx;
+		}
+	}
+	
 	.preview-photo-box {
 		position: fixed;
 		left: 0;
@@ -84,6 +149,8 @@
 				border-radius: 50%;
 			}
 		}
+		
+		
 
 		.download-img {
 			position: absolute;
